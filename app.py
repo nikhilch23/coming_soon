ec78b94666d19fd1567cfad365221f6e0ac01b54
Nikhil <nik.ch.23@gmail.com>
2018-11-11 11:05:44 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import request,render_template,Flask
+import csv
+
+app=Flask('__main__')
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+	if request.method =='POST':
+		name = request.form['name']
+		emailid = request.form['email-id']
+		fieldnames = ['name', 'emailid']
+		with open('list.csv','w') as inFile:
+			writer=csv.DictWriter(inFile, fieldnames=fieldnames)
+			writer.writerow({name:'name', emailid:'emailid'})
+		return "Thanks"	
+
+app.run(port=5002)
